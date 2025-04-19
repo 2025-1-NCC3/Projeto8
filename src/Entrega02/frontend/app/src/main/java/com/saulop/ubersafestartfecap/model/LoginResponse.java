9fffdf525ba8ecd2ea3103480e8db820b5002ddd
Guilhermy Lisboa <guilhermy.lisboa05@gmail.com>
2025-04-19 17:57:15 -0300

    feat: update backend connection #82

--- src/Entrega02/frontend/app/src/main/java/com/saulop/ubersafestartfecap/model/LoginResponse.java
+++ src/Entrega02/frontend/app/src/main/java/com/saulop/ubersafestartfecap/model/LoginResponse.java
@@ -3,6 +3,10 @@ package com.saulop.ubersafestartfecap.model;
 public class LoginResponse {
     private String message;
     private String token;
+    private String username;
+    private String email;
+    private String phone;
+    private String type;
 
     public String getMessage() {
         return message;
@@ -10,5 +14,21 @@ public class LoginResponse {
 
     public String getToken() {
         return token;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getType() {
+        return type;
     }
 }
